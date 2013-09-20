ffed85aef0db581210590b47d6ca7c12e4a592e8
Bill Hunt <bill.hunt@willowtreeapps.com>
2013-09-20 11:41:38 -0400

    Changing translate.sh to use installed version of saxon.

--- translate.sh
+++ translate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 for file in `ls final`; do
 	echo "translating $file"
-	java -Xmx4096m -jar /usr/local/Cellar/saxon/9.5.0.2/libexec/saxon9he.jar -s:final/$file -xsl:translate.xsl
+	saxon -s:final/$file -xsl:translate.xsl
 done
