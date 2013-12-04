opengovfoundation/maryland-state-code

commit 30de7ba58cd5b2831e2102e12d6810db34db046d

Fixing up conversion script.

translate.xsl
@@ -34,38 +34,8 @@
 				</order_by>
 				<!-- TODO: need to nest indented paragraphs -->
 				<text>
-					<xsl:value-of select="text" />
-					<!-- Begin heirarchy -->
-					<!-- Subsection -->
-					<xsl:for-each select="subsection">
-						<section prefix="{current()/enum}">
-							<xsl:value-of select="current()/text" />
-							<!-- Paragraph -->
-							<xsl:for-each select="current()/paragraph">
-								<section prefix="{current()/enum}">
-									<xsl:value-of select="current()/text" />
-									<!-- Subparagraph -->
-									<xsl:for-each select="current()/subparagraph">
-										<section prefix="{current()/enum}">
-											<xsl:value-of select="current()/text" />
-											<!-- Sub-Subparagraph -->
-											<xsl:for-each select="current()/sub-subparagraph">
-												<section prefix="{current()/enum}">
-													<xsl:value-of select="current()/text" />
-													<!-- Sub-Sub-Subparagraph -->
-													<xsl:for-each select="current()/sub-sub-subparagraph">
-														<section prefix="{current()/enum}">
-															<xsl:value-of select="current()/text" />
-														</section>
-													</xsl:for-each>
-												</section>
-											</xsl:for-each>
-										</section>
-									</xsl:for-each>
-								</section>
-							</xsl:for-each>
-						</section>
-					</xsl:for-each>
+					<xsl:copy-of select="current()/text" />
+					<!-- Just get the text in there. -->
 				</text>
 				<!-- TODO add history / metadata -->
 			</law>

clean.sh
@@ -1,36 +1,41 @@
 #!/bin/sh
+
+rm -R 1_stripped
 mkdir -p 1_stripped
 for file in `ls *.xml`; do
 	php ./convert.php $file
 done
 
+rm -R 2_formatted
 mkdir -p 2_formatted
 for file in `ls 1_stripped`; do
 	xmllint -format "1_stripped/$file" -output "2_formatted/$file"
 done
 
+rm -R 3_clean
 mkdir -p 3_clean
 for file in `ls 2_formatted`; do
 	sed '/^<!DOCTYPE.*$/d' "2_formatted/$file" > "3_clean/$file"
 done
 
+rm -R 4_cleaner
 mkdir -p 4_cleaner
-for file in `ls clean`; do
-	art=`echo $file | sed 's/\..*//'`
+for file in `ls 3_clean`; do
+	art=`echo $file | sed 's/\..*$//'`
 	sed -E "s/<article id=\"([^\"]+)\"(\/)?>/<article id=\"$art\"\2>/" "3_clean/$file" > "4_cleaner/$file"
-	sed -i.bak 's/&#x2013;/-/g' "final/$file"
-	sed -i.bak 's/&#x2014;/-/g' "final/$file"
-	sed -i.bak 's/&#x201C;/"/g' "final/$file"
-	sed -i.bak 's/&#x201D;/"/g' "final/$file"
-	sed -i.bak 's/&#xA7;/§/g' "final/$file"
-	sed -i.bak "s/&#x2018;/'/g" "final/$file"
-	sed -i.bak "s/&#x2019;/'/g" "final/$file"
-	sed -i.bak "s/&#x2002;/ /g" "final/$file"
-	sed -i.bak "s/&#x2003;/ /g" "final/$file"
-	sed -i.bak "s/&#x2026;/\.\.\./g" "final/$file"
-	sed -i.bak "s/&#xB0;/º/g" "final/$file"
-	sed -i.bak "s/&#xB5;/µ/g" "final/$file"
-	sed -i.bak "s/&#x2022;/•/g" "final/$file"
-	sed -i.bak "s/--amp--ndash;/-/g" "final/$file"
-	rm final/*.bak
+	sed -i.bak 's/&#x2013;/-/g'      "4_cleaner/$file"
+	sed -i.bak 's/&#x2014;/-/g'      "4_cleaner/$file"
+	sed -i.bak 's/&#x201C;/"/g'      "4_cleaner//$file"
+	sed -i.bak 's/&#x201D;/"/g'      "4_cleaner//$file"
+	sed -i.bak 's/&#xA7;/§/g'        "4_cleaner//$file"
+	sed -i.bak "s/&#x2018;/'/g"      "4_cleaner//$file"
+	sed -i.bak "s/&#x2019;/'/g"      "4_cleaner//$file"
+	sed -i.bak "s/&#x2002;/ /g"      "4_cleaner//$file"
+	sed -i.bak "s/&#x2003;/ /g"      "4_cleaner//$file"
+	sed -i.bak "s/&#x2026;/\.\.\./g" "4_cleaner//$file"
+	sed -i.bak "s/&#xB0;/º/g"        "4_cleaner//$file"
+	sed -i.bak "s/&#xB5;/µ/g"        "4_cleaner//$file"
+	sed -i.bak "s/&#x2022;/•/g"      "4_cleaner//$file"
+	sed -i.bak "s/--amp--ndash;/-/g" "4_cleaner//$file"
+	rm 4_cleaner/*.bak
 done

convert.php
@@ -3,12 +3,14 @@ $unconvertedEntity = array(
 	'&percnt;',
 	'&numsp;',
 	'&lowbar;',
+	'&ndash;',
 	'&#x2013;'
 );
 $convertedChar = array(
 	'%',
 	' ',
 	'_',
+	'-',
 	'-'
 );
 

translate.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+rm -R 5_translated
 mkdir -p 5_translated
+
 for file in `ls 4_cleaner`; do
 	echo "translating $file"
 	saxon -s:4_cleaner/$file -xsl:translate.xsl

commit f2c99fe4212d606c9d206eabd2fc8f311fb153c3

Fixing translation broken in 30de7ba58cd5b2831e2102e12d6810db34db046d.

translate.xsl
@@ -34,7 +34,39 @@
 				</order_by>
 				<!-- TODO: need to nest indented paragraphs -->
 				<text>
-					<xsl:copy-of select="current()/text" />
+				  <xsl:copy-of select="current()/text" />
+
+				  <!-- Begin heirarchy -->
+				  <!-- Subsection -->
+				  <xsl:for-each select="subsection">
+					<section prefix="{current()/enum}">
+					  <xsl:value-of select="current()/text" />
+					  <!-- Paragraph -->
+					  <xsl:for-each select="current()/paragraph">
+						<section prefix="{current()/enum}">
+						  <xsl:value-of select="current()/text" />
+						  <!-- Subparagraph -->
+						  <xsl:for-each select="current()/subparagraph">
+							<section prefix="{current()/enum}">
+							  <xsl:value-of select="current()/text" />
+							  <!-- Sub-Subparagraph -->
+							  <xsl:for-each select="current()/sub-subparagraph">
+								<section prefix="{current()/enum}">
+								  <xsl:value-of select="current()/text" />
+								  <!-- Sub-Sub-Subparagraph -->
+								  <xsl:for-each select="current()/sub-sub-subparagraph">
+									<section prefix="{current()/enum}">
+									  <xsl:value-of select="current()/text" />
+									</section>
+								  </xsl:for-each>
+								</section>
+							  </xsl:for-each>
+							</section>
+						  </xsl:for-each>
+						</section>
+					  </xsl:for-each>
+					</section>
+				  </xsl:for-each>
 					<!-- Just get the text in there. -->
 				</text>
 				<!-- TODO add history / metadata -->